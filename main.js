272a88a0c11aee686817a56bb4cf4e7e6a5d62c9
Prakhar <prakhar0912@gmail.com>
2023-03-29 06:08:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+new Glide('.glide1', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 5,
+    focusAt: 'center',
+    gap: 20,
+    breakpoints: {
+        1000: {
+            perView: 3
+        },
+        470: {
+            perView: 2
+        }
+    }
+    
+}).mount()
+
+new Glide('.glide2', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 5,
+    focusAt: 'center',
+    gap: 20,
+    breakpoints: {
+        1000: {
+            perView: 3
+        },
+        470: {
+            perView: 2
+        }
+    }
+    
+}).mount()
+
+new Glide('.glide3', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 2,
+    focusAt: 'center',
+    gap: 80,
+    breakpoints: {
+        590: {
+            perView: 1
+        }
+    }
+    
+}).mount()
